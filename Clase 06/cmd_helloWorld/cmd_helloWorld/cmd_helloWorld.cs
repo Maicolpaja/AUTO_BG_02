c0d38aa218169442003afebff19a7dcdd16419a9
Renzo Rios <rrios@flesan.local>
2019-08-26 21:25:48 -0500

    Clase 06 - Unica

---
+++ Clase 06/cmd_helloWorld/cmd_helloWorld/cmd_helloWorld.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//NameSpaces
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using Autodesk.Revit.Attributes;
+
+
+
+namespace cmd_helloWorld
+{
+    [Transaction(TransactionMode.ReadOnly)]
+    public class cmd_helloWorld : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, 
+                              ref string message, 
+                              ElementSet elements)
+        {
+            TaskDialog.Show("Mi primer mensaje", "Este es mi primer Hola Mundo");
+            return Result.Succeeded;
+
+        }
+    }
+}
