c50409b8f4f34b0ebf28b5e5109b1cd0b7b7b195
Renzo Rios <rrios@flesan.local>
2019-09-16 22:06:02 -0500

    Clase 09

---
+++ Clase 09/wf_pickInformation/wf_pickInformation/frm_PickInformation.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace wf_pickInformation
+{
+    partial class frm_PickInformation
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frm_PickInformation));
+            this.btn_export = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dgv_Elements = new System.Windows.Forms.DataGridView();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lbl_cant = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lbl_volumen = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_Elements)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_export
+            // 
+            this.btn_export.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btn_export.FlatAppearance.BorderColor = System.Drawing.Color.Blue;
+            this.btn_export.FlatAppearance.BorderSize = 2;
+            this.btn_export.FlatAppearance.MouseDownBackColor = System.Drawing.Color.DeepSkyBlue;
+            this.btn_export.FlatAppearance.MouseOverBackColor = System.Drawing.Color.DeepSkyBlue;
+            this.btn_export.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_export.Font = new System.Drawing.Font("Century Gothic", 10.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_export.ForeColor = System.Drawing.Color.Blue;
+            this.btn_export.Location = new System.Drawing.Point(183, 598);
+            this.btn_export.Name = "btn_export";
+            this.btn_export.Size = new System.Drawing.Size(164, 37);
+            this.btn_export.TabIndex = 0;
+            this.btn_export.Text = "Exportar";
+            this.btn_export.UseVisualStyleBackColor = false;
+            this.btn_export.Click += new System.EventHandler(this.btn_export_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(150, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(231, 23);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "AUTOMATIZACIÓN BIM";
+            // 
+            // dgv_Elements
+            // 
+            this.dgv_Elements.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dgv_Elements.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dgv_Elements.BackgroundColor = System.Drawing.SystemColors.ControlLightLight;
+            this.dgv_Elements.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Comic Sans MS", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgv_Elements.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.dgv_Elements.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv_Elements.Location = new System.Drawing.Point(58, 114);
+            this.dgv_Elements.Name = "dgv_Elements";
+            this.dgv_Elements.RowHeadersVisible = false;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Comic Sans MS", 10.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.Color.Lime;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.dgv_Elements.RowsDefaultCellStyle = dataGridViewCellStyle2;
+            this.dgv_Elements.RowTemplate.Height = 24;
+            this.dgv_Elements.Size = new System.Drawing.Size(415, 415);
+            this.dgv_Elements.TabIndex = 2;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(426, 598);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 93);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lbl_cant
+            // 
+            this.lbl_cant.AutoSize = true;
+            this.lbl_cant.Font = new System.Drawing.Font("Century Gothic", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_cant.Location = new System.Drawing.Point(54, 81);
+            this.lbl_cant.Name = "lbl_cant";
+            this.lbl_cant.Size = new System.Drawing.Size(191, 19);
+            this.lbl_cant.TabIndex = 1;
+            this.lbl_cant.Text = "Elementos seleccionas: 35 ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(315, 81);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 19);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Volumen: ";
+            // 
+            // lbl_volumen
+            // 
+            this.lbl_volumen.AutoSize = true;
+            this.lbl_volumen.Font = new System.Drawing.Font("Century Gothic", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_volumen.ForeColor = System.Drawing.Color.Red;
+            this.lbl_volumen.Location = new System.Drawing.Point(395, 83);
+            this.lbl_volumen.Name = "lbl_volumen";
+            this.lbl_volumen.Size = new System.Drawing.Size(78, 17);
+            this.lbl_volumen.TabIndex = 1;
+            this.lbl_volumen.Text = "120.35 m3";
+            // 
+            // frm_PickInformation
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(527, 691);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.dgv_Elements);
+            this.Controls.Add(this.lbl_volumen);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lbl_cant);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_export);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Name = "frm_PickInformation";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "RESUMEN";
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_Elements)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_export;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dgv_Elements;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lbl_cant;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lbl_volumen;
+    }
+}
---
+++ Clase 09/wf_pickInformation/wf_pickInformation/frm_PickInformation.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using x = Microsoft.Office.Interop.Excel;
+namespace wf_pickInformation
+{
+    public partial class frm_PickInformation : Form
+    {
+        public frm_PickInformation(List<ElementData> data)
+        {
+            InitializeComponent();
+            dgv_Elements.DataSource = data;
+            lbl_cant.Text = "Elementos seleccionados: " + dgv_Elements.Rows.Count.ToString();
+            volumenTotal();
+            //ptb_test.Image = Image.FromFile("../documentos/Imagen.png");
+            //ptb_test.Load("https://playcodeacademy.com/wp-content/uploads/2017/01/codigo-fuente-programacion.jpg");
+        }
+
+
+        private void volumenTotal()
+        {
+            double volumenTotal = 0;
+            foreach (DataGridViewRow row in dgv_Elements.Rows)
+            {
+                volumenTotal = volumenTotal + Convert.ToDouble(row.Cells["Volumen"].Value);
+            }
+            lbl_volumen.Text = volumenTotal.ToString() + " m3";
+        }
+
+        private void btn_export_Click(object sender, EventArgs e)
+        {
+            x.Application excel = new x.Application();
+            excel.Application.Workbooks.Add(true);
+
+            int indiceColumna = 0;
+
+            //Asignar los valores de cabecera de cada columna
+            foreach (DataGridViewColumn col in dgv_Elements.Columns)
+            {
+                indiceColumna++;
+                excel.Cells[1, indiceColumna] = col.Name;
+            }
+
+            int indiceFila = 0;
+
+            //Recorrer como una matriz
+            foreach( DataGridViewRow row in dgv_Elements.Rows)
+            {
+                indiceFila++;
+                indiceColumna = 0;
+
+                foreach (DataGridViewColumn col in dgv_Elements.Columns)
+                {
+                    indiceColumna++;
+                    excel.Cells[indiceFila + 1, indiceColumna] = row.Cells[col.Name].Value;
+                }
+            }
+            excel.Visible = true;
+        }
+    }
+}
